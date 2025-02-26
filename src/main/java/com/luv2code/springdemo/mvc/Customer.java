d1291f6cb2813b2132412ee2b83dcc78b0813010
vrushali_raut <vraut.c@roshn.sa>
2025-02-26 22:49:18 +0530

    Spring mvc validation- apply regex matching

--- src/main/java/com/luv2code/springdemo/mvc/Customer.java
+++ src/main/java/com/luv2code/springdemo/mvc/Customer.java
@@ -1,20 +1,28 @@
 package com.luv2code.springdemo.mvc;
 
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 
 public class Customer {
     private String firstName;
 
-    @NotNull(message="is required")
-    @Size(min=1, message="is required")
+    @NotNull(message = "is required")
+    @Size(min = 1, message = "is required")
     private String lastName;
 
     @Min(value = 0, message = "must be greater than or equal to zero")
     @Max(value = 10, message = "must be greater than or equal to 10")
     private int freePasses;
+
+    @Pattern(regexp = "^[a-zA-Z0-9]{5}", message = "only 5 chars/digits")
+    private String postalCode;
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
 
     public String getFirstName() {
         return firstName;

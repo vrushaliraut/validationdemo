a13b335ed3bbf9d2a916fa5b97cf674945494adc
vrushali_raut <vraut.c@roshn.sa>
2025-02-26 23:21:20 +0530

    Spring mvc validation- custom annotations

--- src/main/java/com/luv2code/springdemo/mvc/Customer.java
+++ src/main/java/com/luv2code/springdemo/mvc/Customer.java
@@ -1,5 +1,6 @@
 package com.luv2code.springdemo.mvc;
 
+import com.luv2code.springdemo.mvc.validation.CourseCode;
 import jakarta.validation.constraints.*;
 
 public class Customer {
@@ -15,6 +16,18 @@ public class Customer {
 
     @Pattern(regexp = "^[a-zA-Z0-9]{5}", message = "only 5 chars/digits")
     private String postalCode;
+
+    // @CourseCode or
+    @CourseCode(value = "TOPS", message = "must start with TOPS")
+    private String courseCode;
+
+    public String getCourseCode() {
+        return courseCode;
+    }
+
+    public void setCourseCode(String courseCode) {
+        this.courseCode = courseCode;
+    }
 
     public String getPostalCode() {
         return postalCode;
